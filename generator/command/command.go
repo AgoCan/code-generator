d33721c0266855a3838fe25ccdd76671ce36555e
Hank <578843228@qq.com>
2024-11-11 10:08:10 +0800

    先能创建文件，文件内容下次补充

--- generator/command/command.go
+++ generator/command/command.go
@@ -1,1 +1,45 @@
 package command
+
+import "embed"
+
+//go:embed templates/build/build.sh.tmpl
+//go:embed templates/cmd/app/options/options.go.tmpl
+//go:embed templates/cmd/app/server.go.tmpl
+//go:embed templates/cmd/main.go.tmpl
+//go:embed templates/config/config.yaml.tmpl
+//go:embed templates/docs/README.md.tmpl
+//go:embed templates/internal/config/config.go.tmpl
+//go:embed templates/internal/log/log.go.tmpl
+//go:embed templates/README.md.tmpl
+var fEmbed embed.FS
+
+var Files = map[string]string{
+	"build/build.sh":                 readFileGetString("templates/build/build.sh.tmpl"),
+	"cmd/app/app/options/options.go": readFileGetString("templates/cmd/app/options/options.go.tmpl"),
+	"cmd/app/app/server.go":          readFileGetString("templates/cmd/app/server.go.tmpl"),
+	"cmd/main.go":                    readFileGetString("templates/cmd/main.go.tmpl"),
+	"config/config.yaml":             readFileGetString("templates/config/config.yaml.tmpl"),
+	"docs/README.md":                 readFileGetString("templates/docs/README.md.tmpl"),
+	"internal/config/config.go":      readFileGetString("templates/internal/config/config.go.tmpl"),
+	"internal/log/log.go":            readFileGetString("templates/internal/log/log.go.tmpl"),
+	"README.md":                      readFileGetString("templates/README.md.tmpl"),
+}
+
+var Dirs = []string{
+	"build",
+	"cmd/app/app/options",
+	"config",
+	"docs",
+	"internal/config",
+	"internal/log",
+}
+
+func readFileGetString(fileName string) string {
+	data, err := fEmbed.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
+}
+
+var Extra = map[string]interface{}{}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/agocan/code-generator/config"
 	"github.com/agocan/code-generator/generator"
 	"github.com/agocan/code-generator/generator/ansible"
+	"github.com/agocan/code-generator/generator/command"
 	"github.com/agocan/code-generator/generator/gitbook"
 	"github.com/agocan/code-generator/generator/mdbook"
 	"github.com/agocan/code-generator/generator/mvc"
@@ -62,6 +63,8 @@ func main() {
 		run(simplehttp.Files, simplehttp.Dirs, simplehttp.Extra)
 	case *config.Item == "mvcgorm":
 		run(mvc.GormFiles, mvc.GormDirs, mvc.GormExtra)
+	case *config.Item == "command":
+		run(command.Files, command.Dirs, command.Extra)
 	default:
 		fmt.Printf("还不支持%v生成器\n", *config.Item)
 	}
