e0a9898f7546dce879b2cc08e426d2f9d98f1aee
agocan <578843228@qq.com>
2024-12-16 06:57:42 -0500

    读embed不需要那么多文件了，直接读一个目录

--- generator/grpc-go/grpc.go
+++ generator/grpc-go/grpc.go
@@ -6,20 +6,7 @@ import (
 	"github.com/go-cheetah/cheetah/generator/gen"
 )
 
-//go:embed templates/api/helloworld.proto.tmpl
-//go:embed templates/cmd/server/options/options.go.tmpl
-//go:embed templates/cmd/server/server.go.tmpl
-//go:embed templates/cmd/main.go.tmpl
-//go:embed templates/conf/conf.yaml.tmpl
-//go:embed templates/internal/conf/common.go.tmpl
-//go:embed templates/internal/conf/conf.go.tmpl
-//go:embed templates/internal/data/data.go.tmpl
-//go:embed templates/internal/service/helloworld.go.tmpl
-//go:embed templates/internal/server.go.tmpl
-//go:embed templates/pb/helloworld/helloworld_grpc.pb.go.tmpl
-//go:embed templates/pb/helloworld/helloworld.pb.go.tmpl
-//go:embed templates/README.md.tmpl
-//go:embed templates/go.mod.tmpl
+//go:embed templates
 var fEmbed embed.FS
 
 func GetFiles() (files map[string]string) {
